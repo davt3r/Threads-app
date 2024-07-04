f832096aed38c0a62a903fea49af2de75846bf95
david <daviidgt03@gmail.com>
2024-07-04 13:36:34 +0200

    fix sign in page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -25,7 +25,10 @@ export default function RootLayout({
       }}
     >
       <html lang='en'>
-        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+      <body className={`${inter.className} bg-dark-1`} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+  {children}
+</body>
+
       </html>
     </ClerkProvider>
   );
